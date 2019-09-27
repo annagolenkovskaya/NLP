dc7908190eb62f7172657fa66b1f450057952907
annagolenkovskaya <42960591+annagolenkovskaya@users.noreply.github.com>
2019-09-27 14:30:43 +0300

    task 3

--- 3.py
+++ 3.py
@@ -12,6 +12,7 @@
 """
 
 import codecs, json, pymorphy2, pandas as pd, re
+from collections import Counter
 
 path = '/home/anna/Desktop/otzovik_medicine.json'
 path_cosmetics = '/home/anna/Desktop/cutted_reviews/all_reviews_texts_lemm_100000.txt'
@@ -32,8 +33,17 @@ with open(rusentilex_path) as text:
 	for line in text:
 		rusentilex.append(line)
 rusentilex = rusentilex[18:]
+
 for line in range(len(rusentilex)):
-	rusentilex[line] = rusentilex[line][:rusentilex[line].find(",")]
+	if 'negative' in rusentilex[line]:
+		rusentilex[line] = rusentilex[line][:rusentilex[line].find(",")] + ' negative'
+	elif 'positive' in rusentilex[line]:
+		rusentilex[line] = rusentilex[line][:rusentilex[line].find(",")] + ' positive'
+	elif 'neutral' in rusentilex[line]:
+		rusentilex[line] = rusentilex[line][:rusentilex[line].find(",")] + ' neutral'
+
+for line in range(len(rusentilex)):
+	rusentilex[line] = re.sub("[^\w]", " ", rusentilex[line]).split()
 
 
 def read_reviews(path):
@@ -92,9 +102,9 @@ def dict_pos(dict, path_to, maturity=False):
 		elif 'INTJ' in tag:
 			dict[line] += '_INTJ'
 
-	# with open(path_to, "w") as f:
-	# 	for r in dict:
-	# 		f.write(r + '\n')
+	with open(path_to, "w") as f:
+		for r in dict:
+			f.write(str(r) + '\n')
 
 	return dict
 
@@ -112,10 +122,10 @@ def bigrams(reviews, path_to, adj_noun=False, part_noun=False, neg_verb=False, n
 	all_noun_noun_ablt_bigrams = 0
 
 	for review in reviews:
-		for word in range(1, len(review[0])):
+		for word in range(1, len(review[1])):
 			words = []
-			words.append(review[0][word - 1])
-			words.append(review[0][word])
+			words.append(review[1][word - 1])
+			words.append(review[1][word])
 			bigrams.append(words)
 
 		res = []
@@ -158,36 +168,93 @@ def bigrams(reviews, path_to, adj_noun=False, part_noun=False, neg_verb=False, n
 			number_of_bigrams += len(res)
 
 	all_res.append(res)
-	number_of_all_bigrams += number_of_bigrams
-	all_bigrams += len(bigrams)
-	all_adj_noun_bigrams += len(adj_noun_bigrams)
-	all_part_noun_bigrams += len(part_noun_bigrams)
-	all_noun_noun_gent_bigrams += len(noun_noun_gent_bigrams)
-	all_noun_noun_ablt_bigrams += len(noun_noun_ablt_bigrams)
-	all_res.append('Всего извлеклось {} биграм'.format(number_of_all_bigrams))
-	all_res.append('Осталось {} биграм'.format(all_bigrams - number_of_all_bigrams))
-	all_res.append('Словосочетаний прилагательное + существительное = {}'.format(all_adj_noun_bigrams))
-	all_res.append('Словосочетаний причастие + существительное = {}'.format(all_part_noun_bigrams))
-	all_res.append('Словосочетаний существительное + существительное в родительном падеже = {}'.
-	           format(all_noun_noun_gent_bigrams))
-	all_res.append('Словосочетаний существительное + существительное в творительном падеже = {}'.
-	           format(all_noun_noun_ablt_bigrams))
+	# number_of_all_bigrams += number_of_bigrams
+	# all_bigrams += len(bigrams)
+	# all_adj_noun_bigrams += len(adj_noun_bigrams)
+	# all_part_noun_bigrams += len(part_noun_bigrams)
+	# all_noun_noun_gent_bigrams += len(noun_noun_gent_bigrams)
+	# all_noun_noun_ablt_bigrams += len(noun_noun_ablt_bigrams)
+	# all_res.append('Всего извлеклось {} биграм'.format(number_of_all_bigrams))
+	# all_res.append('Осталось {} биграм'.format(all_bigrams - number_of_all_bigrams))
+	# all_res.append('Словосочетаний прилагательное + существительное = {}'.format(all_adj_noun_bigrams))
+	# all_res.append('Словосочетаний причастие + существительное = {}'.format(all_part_noun_bigrams))
+	# all_res.append('Словосочетаний существительное + существительное в родительном падеже = {}'.
+	#            format(all_noun_noun_gent_bigrams))
+	# all_res.append('Словосочетаний существительное + существительное в творительном падеже = {}'.
+	#            format(all_noun_noun_ablt_bigrams))
 
-	with open(path_to, "w") as f:
-		for r in all_res:
-			f.write(str(r) + '\n')
+	# with open(path_to, "w") as f:
+	# 	for r in all_res:
+	# 		f.write(str(r) + '\n')
 
 	return all_res
 
 linis_path_to = '/home/anna/Desktop/linis_pos.txt'
 rusentilex_path_to = '/home/anna/Desktop/rusentilex_pos.txt'
 
-path_to = '/home/anna/Desktop/all_bigrams.txt'
+path_to = '/home/anna/Desktop/frequencies_adj_noun.txt'
+
+# dict_pos(rusentilex, rusentilex_path_to)
 reviews = read_reviews(path_cosmetics)
 reviews = reviews[:100]
 for review in reviews:
 	review[0] = re.sub("[^\w]", " ", review[0]).split()
-	review = dict_pos(review[0], linis_path_to, maturity=True)
+	review[1] = re.sub("[^\w]", " ", review[1]).split()
+	review = dict_pos(review[1], linis_path_to, maturity=False)
 
-bigrams = bigrams(reviews, path_to, adj_noun=True, part_noun=True, noun_noun_gent=True, noun_noun_ablt=True,
-                  neg_verb=True)
+bigrams = bigrams(reviews, path_to, adj_noun=True, part_noun=False, noun_noun_gent=False, noun_noun_ablt=False,
+                  neg_verb=False)
+
+
+def rusentilex_tonality(file, bigrams, path_to, pos=False, neg=False):
+	positives = []
+	negatives = []
+
+	for bigram in bigrams:
+		for line in file:
+			if "'" + bigram[1] + "'" in line or "'" + bigram[0] + "'" in line:
+				if pos:
+					if 'positive' in line:
+						positives.append(bigram)
+						break
+
+				if neg:
+					if 'negative' in line:
+						negatives.append(bigram)
+						break
+
+	if pos:
+		for bigram in range(len(positives)):
+			positives[bigram] = positives[bigram][0] + ' ' + positives[bigram][1]
+		counter = Counter(positives)
+		print(type(counter), len(counter), counter)
+		counter_list = []
+		keys = list(counter.keys())
+		for i in range(len(keys)):
+			counter_list.append({keys[i]: counter[keys[i]]})
+		print(counter_list)
+		with open(path_to, "w") as f:
+			for r in counter_list:
+				f.write(str(r) + '\n')
+
+	if neg:
+		for bigram in range(len(negatives)):
+			negatives[bigram] = negatives[bigram][0] + ' ' + negatives[bigram][1]
+		counter = Counter(negatives)
+		counter_list = []
+		keys = list(counter.keys())
+		for i in range(len(keys)):
+			counter_list.append({keys[i]: counter[keys[i]]})
+		with open(path_to, "w") as f:
+			for r in counter_list:
+				f.write(str(r) + '\n')
+
+path = '/home/anna/Desktop/rusentilex_pos.txt'
+path_to_pos = '/home/anna/Desktop/positives.txt'
+path_to_neg = '/home/anna/Desktop/negatives.txt'
+file = []
+with open(path) as text:
+	for line in text:
+		file.append(line)
+
+rusentilex_tonality(file, bigrams[0], path_to_neg, pos=False, neg=True)
