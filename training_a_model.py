a5cdd7e3cfcc5f4b9720a7e970065b823cd7fbcc
annagolenkovskaya <42960591+annagolenkovskaya@users.noreply.github.com>
2020-03-17 19:21:38 +0300

    training a model

--- training_a_model.py
+++ training_a_model.py
@@ -1,24 +1,16 @@
-from flair.data import Corpus, Sentence
-from flair.datasets import WIKINER_RUSSIAN, ColumnCorpus
-from flair.embeddings import TokenEmbeddings, WordEmbeddings, StackedEmbeddings, FlairEmbeddings
-from typing import List
-
-# 1. get the corpus
-# corpus: Corpus = WIKINER_RUSSIAN().downsample(0.05)
-# print(corpus)
+from flair.data import Corpus
+from flair.datasets import ColumnCorpus
+from flair.embeddings import WordEmbeddings
 
 # define columns
-columns = {4: 'text', 1: 'ner'}
+columns = {0: 'text', 1: 'ner'}
 
 # this is the folder in which train, test and dev files reside
-data_folder = '/home/anna/Desktop/markup/brat_data'
+data_folder = '/home/anna/Desktop/markup/brat_data/without_out'
 
 # init a corpus using column format, data folder and the names of the train, dev and test files
 corpus: Corpus = ColumnCorpus(data_folder, columns, train_file='train.txt', test_file='test.txt', dev_file='dev.txt')
 print(corpus)
-print(len(corpus.train[0]))
-print(corpus.train[0].to_tagged_string('ner'))
-print(corpus.train[0].to_tagged_string('text'))
 
 # 2. what tag do we want to predict?
 tag_type = 'ner'
@@ -29,12 +21,6 @@ print(tag_dictionary.idx2item)
 
 # 4. initialize embeddings
 embedding_types = WordEmbeddings('ru-wiki')
-# embedding_types: List[TokenEmbeddings] = [WordEmbeddings('ru-wiki'),
-                                          # comment in this line to use character embeddings
-                                          # CharacterEmbeddings(), # comment in these lines to use flair embeddings
-                                          # FlairEmbeddings('news-forward'), # FlairEmbeddings('news-backward'),
- # ]
-# embeddings: StackedEmbeddings = StackedEmbeddings(embeddings=embedding_types)
 
 # 5. initialize sequence tagger
 from flair.models import SequenceTagger
@@ -48,10 +34,10 @@ from flair.trainers import ModelTrainer
 trainer: ModelTrainer = ModelTrainer(tagger, corpus)
 
 # 7. start training
-trainer.train('/home/anna/Desktop/markup/brat_data/model', learning_rate=0.1, mini_batch_size=32,
+trainer.train('/home/anna/Desktop/markup/brat_data/without_out/model', learning_rate=0.1, mini_batch_size=32,
               max_epochs=150)
 
 # 8. plot weight traces (optional)
 from flair.visual.training_curves import Plotter
 plotter = Plotter()
-plotter.plot_weights('/home/anna/Desktop/markup/brat_data/model/weights.txt')
+plotter.plot_weights('/home/anna/Desktop/markup/brat_data/without_out/model/weights.txt')
